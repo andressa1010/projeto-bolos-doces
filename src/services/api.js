c7b7bcd95deba30ba0684195c5af3527cad6ed77
andressa1010 <andressa.ns10@gmail.com>
2025-02-25 14:35:58 -0300

    api

--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,7 @@ import axios from "axios"
 
 
 const api = axios.create({
-    baseURL:"http://localhost:3000"
+    baseURL:"https://api-avaliacoes.onrender.com"
 })
 
-export default api
+export default api 
